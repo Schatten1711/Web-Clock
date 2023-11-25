c3a1becb9f275d4e3dc92ab9d27ca0365c8bd6fd
Jack <jack@Ubuntu>
2023-11-25 22:34:04 +0000

    edited footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function Footer() {
   return (
     <div className="flex flex-col items-center justify-center pt-60 pb-10">
-      <h1 className="text-sm dark:text-neutral-50">Made By Jack Saunders</h1>
+      <h1 className="text-sm dark:text-neutral-50">
+        Made By Jack (Schatten1711)
+      </h1>
       <p className="text-xs font-light dark:text-neutral-50">
         My Github:{' '}
         <a
